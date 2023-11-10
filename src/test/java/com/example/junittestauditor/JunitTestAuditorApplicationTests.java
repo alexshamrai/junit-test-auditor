09d9cc92b02308abd618eeef552019f5f6ac2d69
Oleksii Shamrai <oshamrai@playtika.com>
2023-11-10 22:33:36 +0200

    Initial commit

---
+++ src/test/java/com/example/junittestauditor/JunitTestAuditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.junittestauditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitTestAuditorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
